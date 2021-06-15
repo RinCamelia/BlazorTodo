2d1e7ab45c038f3abe79dea8d6c407838ef3c11c
Rin Camelia <10214664+RinCamelia@users.noreply.github.com>
2021-06-14 23:07:57 -0500

    Adding todo nav item and empty page

--- obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -90,7 +90,7 @@ using BlazorTodo.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\Shared\NavMenu.razor"
+#line 33 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
