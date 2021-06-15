1cc642e0169e7f0d3d544c88278ed8ad213e764e
Rin Camelia <10214664+RinCamelia@users.noreply.github.com>
2021-06-14 21:59:00 -0500

    Commiting remaining bin and obj changes

--- obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorTodo.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 1 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 2 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 3 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 4 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 5 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 6 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 7 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 8 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 9 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using BlazorTodo;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
+#line 10 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\_Imports.razor"
 using BlazorTodo.Shared;
 
 #line default
@@ -91,13 +91,16 @@ using BlazorTodo.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "c:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\Pages\Counter.razor"
+#line 9 "C:\Users\rinca\OneDrive\Documents\GitHub\BlazorTodo\Pages\Counter.razor"
        
     private int currentCount = 0;
 
+    [Parameter]
+    public int IncrementAmount {get;set;} = 1;
+
     private void IncrementCount()
     {
-        currentCount++;
+        currentCount += IncrementAmount;
     }
 
 #line default
